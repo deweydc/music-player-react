1617700104fee06e1580780783a7245eb40cb7c1
Dewey C <deweycraddock@gmail.com>
2021-01-18 16:54:57 -0600

    Audio Plays

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 //? Import Styles 
 import './styles/app.scss';
 //? Adding Components
 import Player from "./Components/Player";
 import Song from "./Components/Songs";
+//? Import Utilities 
+import data from './library'
 
 function App() {
+  //State 
+  const [songs, setSongs] = useState(data());
+  // returns whole array of song objects 
+  const [currentSong, setCurrentSong] = useState(songs[0])
+  //set the initial song when page is loaded 
+  const [isPlaying, setIsPlaying] = useState(false)
+
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 
-const Player = () => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+    //? Ref
+    const audioRef = useRef(null); //this is how you reference an element in the code. we will use this to reference the audio element below also add from react.
+
+    //? Event Handlers 
+    const playSongHandler = () => {
+        if (isPlaying) {
+            //if the song is playing this will allow pause
+            audioRef.current.pause();
+            setIsPlaying(!isPlaying); // this will switch state isPlaying true or false
+        } else {
+            audioRef.current.play();
+            setIsPlaying(!isPlaying);
+        }
+    }
+
+
+
     return (
         <div className="player">
             <div className="time-control">
@@ -12,12 +29,25 @@ const Player = () => {
                 <p>End Time</p>
             </div>
             <div className="play-control">
-                <FontAwesomeIcon className='rewind' size="2x" icon={faAngleLeft} />
-                <FontAwesomeIcon className='play' size="2x" icon={faPlay} />
-                <FontAwesomeIcon className='fast-forward' size="2x" icon={faAngleRight} />
+                <FontAwesomeIcon
+                    className='rewind'
+                    size="2x"
+                    icon={faAngleLeft}
 
-
+                />
+                <FontAwesomeIcon
+                    className='play'
+                    size="2x"
+                    icon={faPlay}
+                    onClick={playSongHandler} //function will handle playing the song
+                />
+                <FontAwesomeIcon
+                    className='fast-forward'
+                    size="2x"
+                    icon={faAngleRight}
+                />
             </div>
+            <audio ref={audioRef} src={currentSong.audio}></audio>
         </div>
     );
 }
